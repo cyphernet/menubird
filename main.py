1011f8744e1dacd8f114d41c2da7eb053875445d
Matthew Cohen <matt875@gmail.com>
2012-06-23 16:46:18 -0400

    Including some files yo

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import webapp2
 import jinja2
 import os
+from lib.models import *
+from lib.ocr import *
 
 from google.appengine.api import conversion
-from google.appengine.ext import db
+from google.appengine.api import oauth
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-
-class Food(db.Model):
-  name = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
-
-def food_key(food_name=None):
-	return db.Key.from_path('Food', food_name or 'default_food')
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
@@ -21,44 +16,6 @@ class MainPage(webapp2.RequestHandler):
 		template = jinja_environment.get_template('index.html')
 		output = template.render(template_values)
 		self.response.out.write(output)
-
-
-class Ocr(webapp2.RequestHandler):
-	def post(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-
-      # Create a conversion request from HTML to PNG.
-		asset = conversion.Asset("image/png", self.request.get("img"), "image.PNG")
-		conversion_obj = conversion.Conversion(asset, "text/plain", 1, 1, 1, "en-US")
-
-		result = conversion.convert(conversion_obj)
-		if result.assets:
-		  # Note: in most cases, we will return data all in one asset.
-		  # Except that we return multiple assets for multiple pages image.
-		  for asset in result.assets:
-			ocr_text = asset.data
-		  	food_name = unicode(ocr_text, 'utf-8').lower()
-			
-			food = Food()
-			food.name = food_name
-			food.put()
-
-			self.response.out.write(ocr_text)
-
-		else:
-		  handleError(result.error_code, result.error_text)
-
-class Apptest(webapp2.RequestHandler):
-    def get(self):
-		self.response.out.write("""
-			<html>
-				<body>
-		          <form action="/app" enctype="multipart/form-data" method="post">
-		            <div><input type="file" name="img"/></div>
-		            <div><input type="submit" value="test" /></div>
-		          </form>
-		        </body>
-		      </html>""")
 
 app = webapp2.WSGIApplication([('/', MainPage),
                               ('/app', Ocr),
