290ebfa46d93603106c41608370619b607fdb3c6
Matthew Cohen <matt875@gmail.com>
2012-06-23 16:50:12 -0400

    really actually including files

---
+++ lib/models.py
@@ -0,0 +1,8 @@
+﻿from google.appengine.ext import db
+
+class Food(db.Model):
+  name = db.StringProperty(multiline=True)
+  date = db.DateTimeProperty(auto_now_add=True)
+
+def food_key(food_name=None):
+	return db.Key.from_path('Food', food_name or 'default_food')
---
+++ lib/ocr.py
@@ -0,0 +1,38 @@
+﻿import webapp2
+
+class Ocr(webapp2.RequestHandler):
+	def post(self):
+		self.response.headers['Content-Type'] = 'text/plain'
+
+      # Create a conversion request from HTML to PNG.
+		asset = conversion.Asset("image/png", self.request.get("img"), "image.PNG")
+		conversion_obj = conversion.Conversion(asset, "text/plain", 1, 1, 1, "en-US")
+
+		result = conversion.convert(conversion_obj)
+		if result.assets:
+		  # Note: in most cases, we will return data all in one asset.
+		  # Except that we return multiple assets for multiple pages image.
+		  for asset in result.assets:
+			ocr_text = asset.data
+		  	food_name = unicode(ocr_text, 'utf-8').lower()
+			
+			food = Food()
+			food.name = food_name
+			food.put()
+
+			self.response.out.write(ocr_text)
+
+		else:
+		  handleError(result.error_code, result.error_text)
+
+class Apptest(webapp2.RequestHandler):
+    def get(self):
+		self.response.out.write("""
+			<html>
+				<body>
+		          <form action="/app" enctype="multipart/form-data" method="post">
+		            <div><input type="file" name="img"/></div>
+		            <div><input type="submit" value="test" /></div>
+		          </form>
+		        </body>
+		      </html>""")
