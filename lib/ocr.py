eaa481a3d7ef138838c214af7da16ecf60dab6fe
Matthew Cohen <matt875@gmail.com>
2012-06-24 05:38:50 -0400

    added new apis and fixed bugs

--- lib/ocr.py
+++ lib/ocr.py
@@ -5,6 +5,7 @@ from lib.googimagesearch import *
 from google.appengine.api import conversion
 import json
 import sys
+import urllib2
 from fatsecret import FatSecretClient, FatSecretApplication
 from fatsecret import FatSecretError
 from pprint import pprint
@@ -20,69 +21,104 @@ class Ocr(webapp2.RequestHandler):
 	def post(self):
 		self.response.headers['Content-Type'] = 'text/plain'
 		imageFile = self.request.get("img")
-		fileupload = self.request.POST.get("img",None)
-		contentType = getContentType( fileupload.filename )
-		
-		# Create a conversion request from HTML to PNG.
-		asset = conversion.Asset(contentType, imageFile, fileupload.filename)
-		conversion_obj = conversion.Conversion(asset, "text/plain", 1, 1, 1, "en-US")
 
-		result = conversion.convert(conversion_obj)
-		if result.assets:
-		  # Note: in most cases, we will return data all in one asset.
-		  # Except that we return multiple assets for multiple pages image.
-		  for asset in result.assets:
-			ocr_text = asset.data
-		  	food_name = unicode(ocr_text, 'utf-8').lower()
-			
-			q = db.GqlQuery("SELECT * FROM Food " +
-							"WHERE name = :1 " +
-							"ORDER BY date DESC LIMIT 1",
-							food_name)
-			
-			results = q.fetch(10)
-			food_description = []
-			resp_images = []
-			if results:
-			  for x in results:
-				resp_images.append(x.images)
-				food_description.append(x.info)
-			else:
-				# Store the image for laters
-				storage = Filestore()
-				wp = storage.create(imageFile, fileupload.filename, contentType)
-			
-				# self.response.out.write(saved_food.id())
-				# self.response.out.write('\r\n')
-				client = FatSecretClient().connect().setApplication(FatSecretTestApplication)
-				food = (client.foods.search(search_expression=ocr_text, max_results=3))
-				if u'foods' in food:
-					if u'food' in food[u'foods']:
-						foodObj = food[u'foods'][u'food'][0]
-						for f in food[u'foods'][u'food']:
-							if(compStr(f[u'food_name']) == food_name):
-							   foodObj = f
-						food_description.append(foodObj[u'food_description'])
-				
-				ip = self.request.remote_addr
-				#self.response.out.write(ip)
-				goog = GoogImageSearch()
-				res = goog.search(ocr_text, ip)
-				for i in res:
-					resp_images.append(i[u'url'])
-					
-				food = Food()
-				food.name = food_name
-				food.filename = fileupload.filename
-				food.location = 'https://storage.cloud.google.com/menubird/'+fileupload.filename
-				food.images = resp_images
-				food.info = food_description
-				saved_food = food.put()
-					
-			self.response.out.write(json.dumps(dict(word=food_name, images=resp_images, info=food_description)))
-			
+		foodName = self.request.POST.get("word",'')
+		fileupload = self.request.POST.get("img",None)
+		if len( foodName ) > 0 or len( fileupload ) == 0: 
+			food_name = foodName
+			ocr_text = foodName
+			filename = ''
+			contentType = 'text'
 		else:
-		  handleError(result.error_code, result.error_text)
+			
+			try:
+				filename = fileupload.filename
+			except:
+				filename = ''
+			contentType = getContentType( filename )
+
+			# Create a conversion request from HTML to PNG.
+			asset = conversion.Asset(contentType, imageFile, filename)
+			conversion_obj = conversion.Conversion(asset, "text/plain", 1, 1, 1, "en-US")
+
+			result = conversion.convert(conversion_obj)
+			if result.assets:
+			  # Note: in most cases, we will return data all in one asset.
+			  # Except that we return multiple assets for multiple pages image.
+			  for asset in result.assets:
+				ocr_text = asset.data
+				food_name = unicode(ocr_text, 'utf-8').lower()
+			else:
+				handleError(result.error_code, result.error_text)
+		
+		q = db.GqlQuery("SELECT * FROM Food " +
+						"WHERE name = :1 " +
+						"ORDER BY date DESC LIMIT 1",
+						food_name)
+		
+		results = q.fetch(1)
+		
+		resp_images = []
+		if results:
+		  food_description = []
+		  for x in results:
+			resp_images.append(x.images)
+			food_description.append(x.info)
+			self.response.out.write(json.dumps(dict(word=food_name, images=resp_images, info=food_description)))
+		else:
+			# Store the image for laters
+			food_description = {}
+			storage = Filestore()
+			wp = storage.create(imageFile, filename, contentType)
+		
+			# self.response.out.write(saved_food.id())
+			# self.response.out.write('\r\n')
+			client = FatSecretClient().connect().setApplication(FatSecretTestApplication)
+			food = (client.foods.search(search_expression=ocr_text, max_results=3))
+			if u'foods' in food:
+				if u'food' in food[u'foods']:
+					foodObj = food[u'foods'][u'food'][0]
+					for f in food[u'foods'][u'food']:
+						if(compStr(f[u'food_name']) == food_name):
+						   foodObj = f
+					food_description['description'] = foodObj[u'food_description']
+					food_description['food_id'] = foodObj[u'food_id']
+			ip = self.request.remote_addr
+			#self.response.out.write(ip)
+			goog = GoogImageSearch()
+			res = goog.search(ocr_text, ip)
+			for i in res:
+				resp_images.append(i[u'url'])
+			
+			self.response.out.write(json.dumps(dict(word=food_name, images=resp_images, info=food_description)))
+
+			if 'food_id' in food_description:
+				food = client.food.get(food_id=food_description['food_id'])
+				if u'food' in food:
+					if u'servings' in food[u'food']:
+						if u'serving' in food[u'food'][u'servings']:
+							food_description['serving'] = food[u'food'][u'servings'][u'serving']
+			
+			urlFoodName = urllib.quote_plus(foodName)
+			appKey = 'ee031c87dcfd97c3ab60ad494ea9a488'
+			url = 'http://api.pearson.com/kitchen-manager/v1/recipes?name-contains=' + urlFoodName  + '&apikey=' + appKey
+			food_description['recipe'] = urllib2.urlopen(url).read()
+
+			if u'results' in foodName:
+				for r in foodName[u'results']:
+					food_temp = {}
+					for i in r:
+						food_temp[r] = r[i]
+					food_description[r]	= food_temp
+			
+			food = Food()
+			food.name = food_name
+			food.filename = filename
+			food.location = 'https://storage.cloud.google.com/menubird/'+filename
+			food.images = resp_images
+			food.info = json.dumps(food_description)
+			saved_food = food.put()
+					
 
 def getContentType( filename ): # lists and converts supported file extensions to MIME type
 	ext = filename.split('.')[-1].lower()
@@ -99,6 +135,7 @@ class Apptest(webapp2.RequestHandler):
 				<body>
 		          <form action="/app" enctype="multipart/form-data" method="post">
 		            <div><input type="file" name="img"/></div>
+					 <div><input type="text" name="word"/></div>
 		            <div><input type="submit" value="test" /></div>
 		          </form>
 		        </body>
